b5858e4590537c4de9aeed5e112ff7ca4432ac24
Dhil-Web <fadhi199abyan@example.com>
2026-09-01 15:12:40 +0700

    fix: isi supabase url dan anon key valid

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'MASUKKAN_PROJECT_URL_SUPABASE_DI_SINI';
-const supabaseAnonKey = 'MASUKKAN_ANON_PUBLIC_KEY_DI_SINI';
+const supabaseUrl = 'https://tlyeyphchmxuqrefvcoj.supabase.co/rest/v1/';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRseWV5cGhjaG14dXFyZWZ2Y29qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgyNDYxMDEsImV4cCI6MjEwMzgyMjEwMX0.h4wbuTdzSB5TSD7DQTqInS-Pyfp3_WExhY54dpE0QUM';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
